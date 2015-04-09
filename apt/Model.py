5d8423bdda172f8873c4f78ee8f085ad1d787d9e
Nino <njagar@gmail.com>
2015-04-09 19:38:16 +0200

    Model

---
+++ apt/Model.py
@@ -0,0 +1,21 @@
+class Model:
+    def _preprocess_strip(self):
+        return
+
+    def _preprocess_currency(self):
+        return
+
+    def _preprocess_tokenize(self):
+        return
+
+    def _preprocess_compounds(self):
+        return
+
+    def _preprocess_pos_tagging(self):
+        return
+
+    def _preprocess_stopwords(self):
+        return
+
+    def preprocess(self):
+        return
