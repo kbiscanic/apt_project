562cd2d813a4fcbaba39f84ede3f7943aa48b252
njagar <njagar@gmail.com>
2015-04-10 16:46:33 +0200

    preprocessing

--- apt/Main.py
+++ apt/Main.py
@@ -3,7 +3,10 @@ from Model import Model
 model = Model()
 
 # testing
-print model.preprocess("The broader Standard & Poor's 500 Index <.SPX> shed 2.38 points, or 0.24 percent, at 995.10")
-print model.preprocess(
-    "Albuquerque Mayor Martin Chavez said investigators felt confident that with the arrests they had \"at least one of the fires resolved.\"")
-print model.preprocess("Still, he said, \"I\'m absolutely confident we're going to have a bill.\"")
+print model.preprocess([
+    "The broader Standard & Poor's 500 Index <.SPX> shed 2.38 points, or 0.24 percent, at 995.10",
+    "Albuquerque Mayor Martin Chavez said investigators felt confident that with the arrests they had \"at least one of the fires resolved.\""
+])
+print model.preprocess(["cater pillar", "caterpillar"])  # treba ispast [caterpillar] i [caterpillar]
+print model.preprocess(["a b c de ef", "ab bc d e f"])  # treba ispast [ab c de ef] i [ab bc de f]
+print model.preprocess(["$US1234 $US1,234.00 $US345.12 $US -123.13 a sada normalni broj = 213.12", ""])
--- apt/Model.py
+++ apt/Model.py
@@ -1,4 +1,5 @@
 import nltk
+import re
 
 class Model:
     _NLTK_DATA_PATH = '../data/nltk'
@@ -8,35 +9,62 @@ class Model:
             nltk.data.path.append(self._NLTK_DATA_PATH)
 
     def _preprocess_strip(self, sentence):
-        # TODO - da li treba maknuti i . , ? ! [ ] ' "
-        return sentence.strip().translate(None, '\\/-()<>')
+        # return sentence.strip().translate(None, '\\/-()<>')
+        return sentence.strip().translate(None, '\\/-()<>:#[]{}').lower()
 
     def _preprocess_currency(self, sentence):
-        # TODO
-        return sentence
+        return re.sub(r"(\$)[a-zA-Z]+([ 0-9.,]+)", r"\1\2", sentence)
 
     def _preprocess_tokenize(self, sentence):
         return nltk.word_tokenize(sentence)
 
-    def _preprocess_compounds(self, tokens):
-        # TODO
-        return tokens
+    def _preprocess_token_replacement(self, tokens):
+        replace = {'\'m': 'am', 'n\'t': 'not', '\'re': 'are'}
+        return [replace.get(x, x) for x in tokens]
+
+    def _preprocess_compounds(self, tokens1, tokens2):
+        new_tokens1 = []
+        i = 0
+        while i < len(tokens1):
+            if i == len(tokens1) - 1:
+                new_tokens1.append(tokens1[i])
+                break
+            else:
+                compound = tokens1[i] + tokens1[i + 1]
+                if compound in tokens2:
+                    new_tokens1.append(compound)
+                    i += 2
+                else:
+                    new_tokens1.append(tokens1[i])
+                    i += 1
+        return new_tokens1
 
     def _preprocess_pos_tagging(self, tokens):
         return nltk.pos_tag(tokens)
 
     def _preprocess_stopwords(self, tokens):
         stopwords = nltk.corpus.stopwords.words('english')
-        return [x for x in tokens if x[0] not in stopwords]
+        stopwords.extend(['.', ',', '!', '?', '\'', '"', '\'\'', '""', '``'])
+        return [x for x in tokens if x not in stopwords]
 
-    # vraca preprocesirani niz tokena za 1 recenicu
-    def preprocess(self, sentence):
-        sentence = self._preprocess_strip(sentence)
-        sentence = self._preprocess_currency(sentence)
-        tokens = self._preprocess_tokenize(sentence)
-        tokens = self._preprocess_compounds(tokens)
-        tokens = self._preprocess_pos_tagging(tokens)
-        tokens = self._preprocess_stopwords(tokens)
+    # vraca listu koja sadrzi 2 liste preprocesiranih tokena za 1 primjer (2 zadane recenice)
+    def preprocess(self, X):
+        str = ["", ""]
+        tokens = [[], []]
+
+        for i in range(0, 2):
+            str[i] = self._preprocess_strip(X[i])
+            str[i] = self._preprocess_currency(str[i])
+            tokens[i] = self._preprocess_tokenize(str[i])
+            tokens[i] = self._preprocess_token_replacement(tokens[i])
+
+        for i in range(0, 2):
+            tokens[i] = self._preprocess_compounds(tokens[i], tokens[1 - i])
+
+        for i in range(0, 2):
+            tokens[i] = self._preprocess_stopwords(tokens[i])
+            tokens[i] = self._preprocess_pos_tagging(tokens[i])
+
         return tokens
     
     def get_features(self, X):
