86a4a7b87397d73fcecb0d278297e33b4f1dbe23
dkarlo2 <dkarlo2@gmail.com>
2015-05-05 12:54:54 +0200

    added normalized differences

--- apt/karlo/ND.py
+++ apt/karlo/ND.py
@@ -2,6 +2,24 @@
 
 __author__ = 'karlo'
 
+from WWO import calc_ic
 
+
+# sentence length
 def calc_nda(words1, words2):
-    pass
+    l1 = len(words1)
+    l2 = len(words2)
+    if l1 == 0 and l2 == 0:
+        return 0
+    else:
+        return abs(l1 - l2) / max(l1, l2)
+
+
+# the aggregate word information content
+def calc_ndc(words1, words2):
+    ic1 = calc_ic(words1)
+    ic2 = calc_ic(words2)
+    if ic1 == 0 and ic2 == 0:
+        return 0
+    else:
+        return abs(ic1 - ic2) / max(ic1, ic2)
--- apt/karlo/WWO.py
+++ apt/karlo/WWO.py
@@ -37,29 +37,31 @@ def read_data(filename):
     return now, ws
 
 
-def calc_ic(word, data):
+data = read_data("data/word-frequencies.txt")
+
+
+def calc_ic(word):
     return log(data[0], data[1].get(word, 0.1))  # TODO koji broj ima smisla
 
 
-def calc_wwc(words1, words2, data):
+def calc_wwc(words1, words2):
     w1 = set(words1)
     w2 = set(words2)
 
     sum1 = 0
     for w in w1 & w2:
-        sum1 += calc_ic(w, data)
+        sum1 += calc_ic(w)
 
     sum2 = 0
     for w in w2:
-        sum2 += calc_ic(w, data)
+        sum2 += calc_ic(w)
 
     return sum1 / sum2
 
 
 def calc_wwo(words1, words2):
-    data = read_data('data/word-frequencies.txt')
-    wwc1 = calc_wwc(words1, words2, data)
-    wwc2 = calc_wwc(words2, words1, data)
+    wwc1 = calc_wwc(words1, words2)
+    wwc2 = calc_wwc(words2, words1)
     if wwc1 + wwc2 == 0:
         return 0
     else:
