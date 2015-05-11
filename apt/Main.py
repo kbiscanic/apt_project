a74e29faa0fe73849802ff74737aed09b1aaba5f
njagar <njagar@gmail.com>
2015-05-11 19:11:31 +0200

    output

--- apt/Main.py
+++ apt/Main.py
@@ -38,13 +38,29 @@ def load_data_y(file):
         y.append(float(line))
     return y
 
+
+# ispisuje izlaz modela u datoteku (rezultati se ogranicavaju na [0, 5])
+def write_output(file, output):
+    f = open(file, 'w')
+    for x in output:
+        if x < 0:
+            r = 0
+        elif x > 5:
+            r = 5
+        else:
+            r = x
+        f.write(str(r) + '\n')
+
 X_train = load_data_X('../data/train/STS.input.MSRpar.txt')
 y_train = load_data_y('../data/train/STS.gs.MSRpar.txt')
 X_test = load_data_X('../data/test-gold/STS.input.MSRpar.txt')
 y_test = load_data_y('../data/test-gold/STS.gs.MSRpar.txt')
 # model = Model()
 # model.train(X_train, y_train)
-# model.train_k_fold(X_train, y_train, [1, 2, 3], [1, 2, 3], [0.1, 0.2, 0.3], 3)
+# C_set = [2**x for x in range(-5, 15 + 1)]
+# gamma_set = [2**x for x in range(-15, 3 + 1)]
+# epsilon_set = [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+# model.train_k_fold(X_train, y_train, C_set, gamma_set, epsilon_set, 3)
 # print "Tocni: ", y_train
 # ove dodatne debug poruke ispisuje sam library, a MSE nije tocan (jer se nije predao pravi y u predict, ali se moze)
 # print "Dobiveni: ", model.predict(X_train)
