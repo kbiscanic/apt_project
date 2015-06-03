edf661e4657645967472e653183f4de1c1ad87c4
njagar <njagar@gmail.com>
2015-06-03 12:55:19 +0200

    ispis najlosijih n primjera

--- apt/Main.py
+++ apt/Main.py
@@ -38,20 +38,41 @@ def load_data_y(file):
         y.append(float(line))
     return y
 
-# ispisuje izlaz modela u datoteku (rezultati se ogranicavaju na [0, 5])
+
+# ogranicava vrijednost x izmedju minx i maxx
+def clamp(x, minx, maxx):
+    return max(min(maxx, x), minx)
+
+
+# ispisuje izlaz modela u datoteku file (rezultati se ogranicavaju na [0, 5])
 def write_output(file, output):
     f = open(file, 'w')
     for x in output:
-        if x < 0:
-            r = 0
-        elif x > 5:
-            r = 5
-        else:
-            r = x
+        r = clamp(x, 0, 5)
         f.write(str(r) + '\n')
 
+
+# ispisuje n najgore ocjenjenih primjera u datoteku file (rezultati se ogranicavaju na [0, 5])
+def write_low_scored(file, X, y, output, n):
+    if (n >= len(output)):
+        n = len(output) - 1
+    diffs = [abs(clamp(output[i], 0, 5) - y[i]) for i in xrange(0, len(output))]
+    diff_threshold = sorted(diffs, reverse=True)[n]
+
+    f = open(file, 'w')
+    for i in xrange(0, len(output)):
+        r = clamp(output[i], 0, 5)
+        diff = abs(r - y[i])
+        if diff >= diff_threshold:
+            f.write(X[i][0].encode('utf-8') + '\n')
+            f.write(X[i][1].encode('utf-8') + '\n')
+            f.write('Tocno: ' + str(y[i]) + '\n')
+            f.write('Dobiveno: ' + str(r) + '\n')
+            f.write('Razlika: ' + str(diff) + '\n')
+
 # testiranje za 1 primjer
-def test(X_train_file, y_train_file, X_test_file, y_test_file, train_out_file, test_out_file):
+def test(X_train_file, y_train_file, X_test_file, y_test_file, train_out_file, test_out_file, train_bad_out_file=None,
+         test_bad_out_file=None):
     X_train = load_data_X(X_train_file)
     y_train = load_data_y(y_train_file)
     X_test = load_data_X(X_test_file)
@@ -61,17 +82,21 @@ def test(X_train_file, y_train_file, X_test_file, y_test_file, train_out_file, t
     C_set = [2 ** x for x in range(-5, 15 + 1)]
     gamma_set = [2 ** x for x in range(-15, 3 + 1)]
     epsilon_set = [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
-    model.train_k_fold(X_train, y_train, C_set, gamma_set, epsilon_set, 3)
+    model.train_k_fold(X_train, y_train, C_set, gamma_set, epsilon_set, 10)
 
     print "Tocni (train): ", y_train
     predicted_train = model.predict(X_train)
     print "Dobiveni (train): ", predicted_train
     write_output(train_out_file, predicted_train)
+    if (train_bad_out_file is not None):
+        write_low_scored(train_bad_out_file, X_train, y_train, predicted_train, 30)
 
     print "Tocni (test): ", y_test
     predicted_test = model.predict(X_test)
     print "Dobiveni (test): ", predicted_test
     write_output(test_out_file, predicted_test)
+    if (test_bad_out_file is not None):
+        write_low_scored(test_bad_out_file, X_test, y_test, predicted_test, 30)
 
     print 'C:', model.get_param_C()
     print 'epsilon:', model.get_param_epsilon()
@@ -79,13 +104,12 @@ def test(X_train_file, y_train_file, X_test_file, y_test_file, train_out_file, t
 
 # test('../data/train/STS.input.MSRpar.txt', '../data/train/STS.gs.MSRpar.txt',
 #     '../data/test-gold/STS.input.MSRpar.txt', '../data/test-gold/STS.gs.MSRpar.txt',
-#     'MSRpar_train.out', 'MSRpar_test.out')
+    # 'MSRpar_train.out', 'MSRpar_test.out', 'MSRpar_train_bad.txt', 'MSRpar_test_bad.txt')
 
 # test('../data/train/STS.input.MSRvid.txt', '../data/train/STS.gs.MSRvid.txt',
 #     '../data/test-gold/STS.input.MSRvid.txt', '../data/test-gold/STS.gs.MSRvid.txt',
-#     'MSRvid_train.out', 'MSRvid_test.out')
+#     'MSRvid_train.out', 'MSRvid_test.out', 'MSRvid_train_bad.txt', 'MSRvid_test_bad.txt')
 
-# test('../data/train/STS.input.SMTeuroparl.txt', '../data/train/STS.gs.SMTeuroparl.txt',
+    # test('../data/train/STS.input.SMTeuroparl.txt', '../data/train/STS.gs.SMTeuroparl.txt',
 # '../data/test-gold/STS.input.SMTeuroparl.txt', '../data/test-gold/STS.gs.SMTeuroparl.txt',
-#     'SMTeuroparl_train.out', 'SMTeuroparl_test.out')
-
+    #     'SMTeuroparl_train.out', 'SMTeuroparl_test.out', 'SMTeuroparl_train_bad.txt', 'SMTeuroparl_test_bad.txt')
