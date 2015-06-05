15d3da7703e5da0f406dbe5ee9f2a21109924f8d
dkarlo2 <dkarlo2@gmail.com>
2015-06-05 11:30:15 +0200

    popravljen word2vec

--- apt/features/karlo/word2vec.py
+++ apt/features/karlo/word2vec.py
@@ -31,23 +31,5 @@ def calc_w2v_similarity(words):
 
     return dot(matutils.unitvec(array(vec1)), matutils.unitvec(array(vec2)))
 
-
-'''
-    if len(words1) == 0 or len(words2) == 0:
-        return 0
-    sim = 0.0
-    n = 0
-    for w1 in words1:
-        for w2 in words2:
-            try:
-                sim += w2v_model.similarity(w1, w2)
-                n += 1
-            except KeyError:
-                pass
-    if n == 0:
-        return [0]
-    sim /= n
-    return [sim]'''
-
 # w2v_model = Word2Vec.load_word2vec_format("features/karlo/GoogleNews-vectors-negative300.bin", binary=True)
 w2v_model = Word2Vec.load_word2vec_format("features/karlo/vectors.6B.50d.txt", binary=False)
