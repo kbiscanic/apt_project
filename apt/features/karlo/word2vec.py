c56263ca751970552fbbe4771e0c76f793209642
dkarlo2 <dkarlo2@gmail.com>
2015-06-05 11:29:07 +0200

    popravlje word2vec

--- apt/features/karlo/word2vec.py
+++ apt/features/karlo/word2vec.py
@@ -1,11 +1,38 @@
 __author__ = 'karlo'
 
 from gensim.models import Word2Vec
+from gensim import matutils
+from numpy import dot, array
+
+from WWO import calc_ic
+
+
+def sentence_vec(words):
+    vec = None
+    for w in words:
+        try:
+            v = w2v_model[w]
+            ic = calc_ic(w)
+            if vec is None:
+                vec = [ic * v[i] for i in range(len(v))]
+            else:
+                vec = [ic * v[i] + vec[i] for i in range(len(v))]
+        except KeyError:
+            pass
+    return vec
 
 
 def calc_w2v_similarity(words):
     words1 = words[0]
     words2 = words[1]
+
+    vec1 = sentence_vec(words1)
+    vec2 = sentence_vec(words2)
+
+    return dot(matutils.unitvec(array(vec1)), matutils.unitvec(array(vec2)))
+
+
+'''
     if len(words1) == 0 or len(words2) == 0:
         return 0
     sim = 0.0
@@ -20,7 +47,7 @@ def calc_w2v_similarity(words):
     if n == 0:
         return [0]
     sim /= n
-    return [sim]
+    return [sim]'''
 
 # w2v_model = Word2Vec.load_word2vec_format("features/karlo/GoogleNews-vectors-negative300.bin", binary=True)
 w2v_model = Word2Vec.load_word2vec_format("features/karlo/vectors.6B.50d.txt", binary=False)
